67b69169ae4763c4ac24b245c1566822d4c9f45d
Fredd <fredd@neosdev.com.br>
2018-08-24 20:17:45 -0300

    fim do crud produtos

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -18,28 +18,97 @@ exports.post = (req, res, next) => {
       })
     })
 }
-
 exports.put = (req, res, next) => {
 // devolvendo o status de ok mais informações
-  const id = req.params.id
-  res.status(200).send({
-    item: id,
-    message: 'Metodo Put'
-  })
+  Product
+    .findByIdAndUpdate(req.body.id, {
+      $set: {
+        title: req.body.title,
+        description: req.body.description,
+        price: req.body.price,
+        slug: req.body.slug
+      }
+    }).then(x => {
+      res.status(200).send({
+        message: 'Produto Atualizado com sucesso!'
+      })
+    }).catch(e => {
+      res.status(400).send({
+        message: 'Não foi possivel atualizar o produto',
+        data: e
+      })
+    })
 }
 
-exports.delete =  (req, res, next) => {
+exports.delete = (req, res, next) => {
 // devolvendo o status de ok mais informações
-  res.status(200).send({
-    item: req.params.id,
-    message: 'Metodo Delete'
-  })
+  Product
+    .findByIdAndRemove(req.body.id)
+    .then(x => {
+      res.status(200).send({
+        message: 'Produto Excluido com sucesso!'
+      })
+    }).catch(e => {
+      res.status(400).send({
+        message: 'Não foi possivel excluir o produto',
+        data: e
+      })
+    })
 }
 
 exports.get = (req, res, next) => {
 // devolvendo o status de ok mais informações
-  res.status(200).send({
-    item: 'only a get',
-    message: 'Metodo Get'
+  Product.find({})
+    .then(data => {
+      res.status(200).send(data)
+    }).catch(e => {
+      res.status(400).send({
+        message: 'Erro ao processar sua requisição',
+        data: e
+      })
+    })
+}
+
+exports.getBySlug = (req, res, next) => {
+// devolvendo o status de ok mais informações
+  Product.findOne({
+    slug: req.params.slug,
+    active: true
   })
+    .then(data => {
+      res.status(200).send(data)
+    }).catch(e => {
+      res.status(400).send({
+        message: 'Erro ao processar sua requisição',
+        data: e
+      })
+    })
+}
+exports.getById = (req, res, next) => {
+// devolvendo o status de ok mais informações
+  Product.findById(req.params.id)
+    .then(data => {
+      res.status(200).send(data)
+    }).catch(e => {
+      res.status(400).send({
+        message: 'Erro ao processar sua requisição',
+        data: e
+      })
+    })
+}
+
+exports.getByTag = (req, res, next) => {
+  // devolvendo o status de ok mais informações
+  Product.find({
+    tags: req.params.tag,
+    active: true
+  })
+    .then(data => {
+      res.status(200).send(data)
+    }).catch(e => {
+      res.status(400).send({
+        message: 'Erro ao processar sua requisição',
+        data: e
+      })
+    })
 }
--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -4,11 +4,17 @@ const controller = require('../controllers/productController')
 
 // rota principal
 router.get('/', controller.get)
+// rota principal
+router.get('/:slug', controller.getBySlug)
+// rota principal
+router.get('/admin/:id', controller.getById)
+router.get('/tags/:tag', controller.getByTag)
+
 // rota de post
 router.post('/', controller.post)
 // Rota de put
-router.put('/:id', controller.put)
+router.put('/', controller.put)
 // rota de delete
-router.delete('/:id',controller.delete)
+router.delete('/', controller.delete)
 // o que eu quero exportar
 module.exports = router
